0a02bd2a456ba8fc5b9d40120d540f33621c27f0
Mohanappriya <mohanappriyakarunakaran@gmail.com>
2026-01-17 19:49:25 +0530

    fix: use env-based API URL for frontend

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,8 @@ app.use(
         "http://localhost:5174",
         "http://localhost:5175",
         "http://localhost:5176",
-        "http://localhost:5177"
+        "http://localhost:5177",
+        "http://localhost:5178"
       ];
 
       // Allow server-to-server / Postman / mobile apps
